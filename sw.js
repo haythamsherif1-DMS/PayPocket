1dc6d623c1cdf259a6bbb5b86600661389745d81
haythamsherif1-DMS <haythamsherif1@gmail.com>
2026-04-19 16:58:13 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'smart-payroll-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/512.jpg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
